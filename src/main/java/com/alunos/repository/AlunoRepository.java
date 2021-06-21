73eca37abbd917c7a0bc0b7410d4ec29b4a68207
Wellingtonpleni <wellingtonpleni@hotmail.com>
2021-06-21 13:37:59 -0300

    Início Backend

---
+++ src/main/java/com/alunos/repository/AlunoRepository.java
@@ -0,0 +1,11 @@
+package com.alunos.repository;
+
+import com.alunos.entities.Aluno;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<Aluno,Long> {
+    
+
+
+}
